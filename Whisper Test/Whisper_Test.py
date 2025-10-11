8b1a62c74cc09ac5fcc998f7a9197e31326f63e4
Engebret <engebret@hotmail.no>
2025-10-11 15:36:09 +0200

    WHisper test files
    Basic whisper code, as well as the audio clips and their respective transcripts

---
+++ Whisper Test/Whisper_Test.py
@@ -0,0 +1,26 @@
+import os
+import whisper
+
+model = whisper.load_model("base")
+
+AUDIO_DIR = "Audio_Jungle"
+
+OUTPUT_DIR = "transcript"
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+EXTS = (".m4a", ".mp3")
+
+for filename in os.listdir(AUDIO_DIR):
+    if filename.lower().endswith(EXTS):
+        filepath = os.path.join(AUDIO_DIR, filename)
+        print(f"Transcribing {filename}...")
+
+        result = model.transcribe(filepath)
+        text = result["text"].strip()
+
+        outpath = os.path.join(OUTPUT_DIR, f"{os.path.splitext(filename)[0]}.txt")
+
+        with open(outpath, "w", encoding="utf-8") as f:
+            f.write(text)
+        
+        print(f"Done  with {filename} and its now {outpath}")
