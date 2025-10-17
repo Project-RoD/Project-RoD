0cd4a0a0172ee876aeca978d046d1efa2a65d1e4
Norwegionaire <engebret@hotmail.no>
2025-10-17 15:50:38 +0200

    Testing Failed, skal prøve noe etterpå

--- Code/testing/speech-to-text/whisper_test.py
+++ Code/testing/speech-to-text/whisper_test.py
@@ -1,20 +1,24 @@
 import os
-from openai import OpenAI
+from openai import AsyncOpenAI
 from dotenv import load_dotenv
+from pathlib import Path
 """
 import whisper
 
 model = whisper.load_model("base")
 """
-load_dotenv()
+FILE_PATH = Path(__file__).parent.resolve()
+
+load_dotenv(dotenv_path="/home/bretski/Documents/Project-RoD/Code/rod/tests/.env")
+print("Key loaded?", os.getenv("OPENAI_API_KEY"))
 
 API_KEY = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=API_KEY)
+client = AsyncOpenAI(api_key=API_KEY)
 
 MODEL = "whisper-1"
 
-AUDIO_DIR = "audio_files"
-OUTPUT_DIR = "transcript"
+AUDIO_DIR = FILE_PATH / "audio_files"
+OUTPUT_DIR = FILE_PATH / "transcript"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 EXTS = (".m4a", ".mp3")
