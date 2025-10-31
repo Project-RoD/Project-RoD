c98fef51749e69249b6339e4b4d1e7e40d3e7452
Norwegionaire <engebret@hotmail.no>
2025-10-31 17:01:54 +0100

    Final

--- Code/testing/text-to-speech/text-to-speech_test.py
+++ Code/testing/text-to-speech/text-to-speech_test.py
@@ -7,13 +7,13 @@ load_dotenv(dotenv_path="/home/bretski/Documents/Project-RoD/Code/rod/tests/.env
 client = ElevenLabs(api_key=os.getenv("ELEVEN_LABS_API_KEY"))
 print("Loaded key:", os.getenv("ELEVEN_LABS_API_KEY"))
 
-text = "Hei der! Hvordan går det i dag? Jeg håper du har en fin dag."
+text = "Hei! Eg er glad i brunost. Har du ete i dag?"
 
 def text_to_speech_device(chat_text, filename = "output.mp3"):
     from elevenlabs import save
 
     audio = client.text_to_speech.convert(
-        voice_id="d6dMXDDMQ9zlhV3hOfx0",
+        voice_id="s2xtA7B2CTXPPlJzch1v",
         model_id="eleven_flash_v2_5",
         text=chat_text,
         output_format="mp3_44100_128"
@@ -21,7 +21,6 @@ def text_to_speech_device(chat_text, filename = "output.mp3"):
 
     save(audio, filename)
     return filename
-
 
 time_start = time.time()
 text_to_speech_device(text)
