f6b3a532f272de758a0892b7ee4b49d99c2c77e0
Norwegionaire <engebret@hotmail.no>
2025-11-03 11:00:44 +0100

    update, still not working

--- Code/testing/Connected.py
+++ Code/testing/Connected.py
@@ -2,9 +2,12 @@ import os, sys
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(dotenv_path="/home/bretski/Documents/Project-RoD/Code/rod/tests/.env")
 
 BASE_DIR = Path(__file__).resolve().parent[1]
 
-sys.path.append(BASE_DIR / "whisper_test")
-sys.path.append(BASE_DIR / "grammar_check_test")
+sys.path.append(str(BASE_DIR / "whisper_test"))
+sys.path.append(str(BASE_DIR / "grammar_check_test"))
+
+import whisper_test
+import gramma_check_test
